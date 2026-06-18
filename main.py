7ae35d8060fa42298a358ba2322e585c5ce06567
willepalmberg <wille2008080808@gmail.com>
2026-06-18 21:56:57 +0200

    Extrahera emojis från fontfilen

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from Backend.agent import Agent
+from fontTools.ttLib import TTFont
 from PIL import Image
 
 FONT_PATH = "Backend/Assets/NotoColorEmoji-Regular.ttf"
 
+font = TTFont("your_font.ttf")
+cmap = font.getBestCmap()
+characters = [chr(code) for code in cmap.keys()]
+
 im = Image.open("test_image.jpg")
 
-agent = Agent(im, FONT_PATH, 20)
+agent = Agent(im, font, 20)
 agent.place_random_emoji()
