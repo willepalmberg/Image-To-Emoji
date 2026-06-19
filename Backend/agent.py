e9d7f409013839e1bec38e29d261bd2969d5c7b7
willepalmberg <wille2008080808@gmail.com>
2026-06-19 12:39:55 +0200

    Hittade ett library att rendera emojis

--- Backend/agent.py
+++ Backend/agent.py
@@ -1,28 +1,27 @@
-from PIL import Image, ImageFont, ImageText
+from PIL import Image, ImageFont, ImageText, ImageDraw
+from pilmoji import Pilmoji
 from random import randint, choice
 from string import ascii_letters
 
 
 class Agent:
-    def __init__(self, image: Image.Image, emoji_font, emoji_size):
+    def __init__(self, image: Image.Image, font, emojis: list[str], emoji_size):
         self.image = image
         self.final_image = None
-        self.font = emoji_font
+        self.emojis = emojis
+        self.font = font
         self.font_size = emoji_size
 
         self.letters = ascii_letters
 
     def place_random_emoji(self) -> Image.Image:
-        if self.final_image is not None:
-            return self.final_image
+        image = self.image.copy().convert("RGBA")
 
-        image = self.image.copy()
-        im_font = ImageFont.truetype(self.font, self.font_size)
-        text = ImageText.Text(choice(self.letters), im_font)
-        text.embed_color()
-        text.stroke(2, "#0f0")
-        im_text = Image.new("RGB", text.get_bbox()[2:])
-        image.paste(im_text, (randint(0, image.width - im_text.width), randint(0, image.height - image.height)))
-        image.show()
-        return Image
+        font = ImageFont.truetype(self.font, self.font_size)
+        chosen = choice(self.emojis)
+
+        with Pilmoji(image) as pilmoji:
+            pilmoji.text((10, 10), chosen.strip(), (0, 0, 0), font)
+
+        return image
 
--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from Backend.agent import Agent
 from fontTools.ttLib import TTFont
 from PIL import Image
 
+
 FONT_PATH = "Backend/Assets/NotoColorEmoji-Regular.ttf"
 
-font = TTFont("your_font.ttf")
+font = TTFont(FONT_PATH)
 cmap = font.getBestCmap()
-characters = [chr(code) for code in cmap.keys()]
+emojis = [chr(code) for code in cmap.keys()]
+font.close()
 
 im = Image.open("test_image.jpg")
 
-agent = Agent(im, font, 20)
-agent.place_random_emoji()
+agent = Agent(im, FONT_PATH, emojis, 20)
+im = agent.place_random_emoji()
+for i, x in enumerate(range(100)):
+    a = Agent(im, FONT_PATH, emojis, 20)
+    im = a.place_random_emoji()
+    print(f"{i+1}/100")
+im.show()
+
