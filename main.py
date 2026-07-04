e32c8c0dec5df1e1432c6e56cfec923d1f948fd1
willepalmberg <wille2008080808@gmail.com>
2026-07-04 13:43:30 +0200

    Konvertera svgs till pngs

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from io import BytesIO
+
 import cv2
 import numpy
 from PIL import Image
@@ -7,10 +9,17 @@ import requests
 import os
 import zipfile
 import shutil
+import cairosvg
+from io import BytesIO
 
 # Här får vi våra svg emojis ifrån
 TWEMOJI_URL = "https://github.com/twitter/twemoji/archive/refs/tags/v14.0.2.zip"
+TWEMOJI_SVGS_PATH = 'twemoji-14.0.2/assets/svg/'
 
+EMOJI_PATH_SVG = 'Backend/Assets/Emojis_SVG'
+EMOJI_PATH_PNG = 'Backend/Assets/Emojis_PNG'
+
+EMOJI_SIZE = 16
 
 # Från https://stackoverflow.com/questions/15644964/python-progress-bar-and-downloads
 def download_emojis():
@@ -19,7 +28,7 @@ def download_emojis():
         response = requests.get(TWEMOJI_URL, stream=True)
         total_length = response.headers.get('content-length')
 
-        if total_length is None:  # no content length header
+        if total_length is None:
             f.write(response.content)
         else:
             dl = 0
@@ -31,25 +40,53 @@ def download_emojis():
                 sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
                 sys.stdout.flush()
 
-def extract_emojis():
-    print(f'Extracting emojis.zip to {emoji_path}')
 
-    if not os.path.exists(emoji_path):
-        os.makedirs(emoji_path)
+def extract_emojis():
+    print(f'Extracting emojis.zip to {EMOJI_PATH_SVG}')
+
+    if not os.path.exists(EMOJI_PATH_SVG):
+        os.makedirs(EMOJI_PATH_SVG)
 
     z = zipfile.ZipFile('emojis.zip')
-    prefix = 'twemoji-14.0.2/assets/svg/'
 
     for file in z.namelist():
-        if file.startswith(prefix) and file != prefix:
+        if file.startswith(TWEMOJI_SVGS_PATH) and file != TWEMOJI_SVGS_PATH:
             filename = os.path.basename(file)
             if filename:
-                target_path = os.path.join(emoji_path, filename)
+                target_path = os.path.join(EMOJI_PATH_SVG, filename)
                 with z.open(file) as source, open(target_path, 'wb') as target:
                     shutil.copyfileobj(source, target)
 
-start_data = cv2.imread('test_image.jpg')
-start_img = Image.fromarray(start_data)
+def convert_svgs_to_pngs():
+    if not os.path.exists:
+        os.makedirs(EMOJI_PATH_PNG)
+
+    for filename in os.listdir(EMOJI_PATH_SVG):
+        if filename.endswith('.svg'):
+            svg_path = os.path.join(EMOJI_PATH_SVG, filename)
+            png_path = os.path.join(EMOJI_PATH_PNG, filename.replace('.svg', '.png'))
+
+            bytes = cairosvg.svg2png(
+                url=svg_path,
+                output_width=EMOJI_SIZE,
+                output_height=EMOJI_SIZE,
+            )
+
+            rendered_emoji = Image.open(BytesIO(bytes)).convert('RGBA')
+
+            w, h = rendered_emoji.size
+
+            if w != EMOJI_SIZE or h != EMOJI_SIZE:
+                scale = min(EMOJI_SIZE / w, EMOJI_SIZE / h)
+                new_w, new_h = int(w * scale), int(h * scale)
+                rendered_emoji_image = rendered_emoji.resize((new_w, new_h), Image.LANCZOS)
+
+            canvas = Image.new('RGBA', (EMOJI_SIZE, EMOJI_SIZE), (0, 0, 0, 0))
+            paste_x = (EMOJI_SIZE - new_w) // 2
+            paste_y = (EMOJI_SIZE - new_h) // 2
+            canvas.paste(rendered_emoji_image, (paste_x, paste_y))
+
+            canvas.save(png_path)
 
 if os.path.isfile('emojis.zip'):
     print("emojis.zip already exists")
@@ -57,8 +94,7 @@ else:
     print("emojis.zip does not exist")
     download_emojis()
 
-emoji_path = 'Backend/Assets/Emojis_SVG'
-if len(os.listdir(emoji_path)) == 0:
+if len(os.listdir(EMOJI_PATH_SVG)) == 0:
     extract_emojis()
 
 else:
